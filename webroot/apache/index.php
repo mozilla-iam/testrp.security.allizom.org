63897b0d064776a067edf2bc5f157a31c42486e4
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2016-11-17 14:31:15 -0800

    new base root for apache that has the same themes etc. as nginx

--- webroot/apache/index.php
+++ webroot/apache/index.php
@@ -19,12 +19,12 @@
         <div id="main-content">
             <h1>Authentication status</h1>
 <?php
-                echo "Logged in as: ".$_SERVER['HTTP_REMOTE_USER'].", also known as ".$_SERVER['HTTP_OIDC_CLAIM_ID_TOKEN_NAME']." ";
+                echo "Logged in as: ".$_SERVER['REMOTE_USER'].", also known as ".$_SERVER['OIDC_CLAIM_ID_TOKEN_NAME']." ";
 ?>
-            (<a href="/logout">Logout</a>)<br/>
-            Your id_token (and session) will expire on: <?php echo date("d F Y H:i:s", $_SERVER['HTTP_OIDC_CLAIM_ID_TOKEN_EXP']); ?> UTC
-            </br>
-            </br>
+            (<a href="/logout">Logout</a>) (<a href="https://testrp.security.allizom.org">Go back to testrp.security.allizom.org / main page</a>)<br/>
+            Your id_token (and session) will expire on: <?php echo date("d F Y H:i:s", $_SERVER['OIDC_CLAIM_ID_TOKEN_EXP']); ?> UTC
+            <br/>
+            <br/>
 
             <p>
             This test setup and complete configuration can be found at: <a href="https://github.com/mozilla-iam/testrp.security.allizom.org">https://github.com/mozilla-iam/testrp.security.allizom.org</a>
@@ -34,8 +34,8 @@
             <pre>
 <?php
             foreach($_SERVER as $key => $value) {
-                    if (substr($key, 0, 5) === "HTTP_") {
-                        echo $key.":".$value."</br>";
+                    if (substr($key, 0, 5) === "OIDC_") {
+                        echo $key.":".$value."<br/>";
                     }
             }
 ?>
@@ -44,10 +44,10 @@
             <h2>Endpoints</h2>
 
             <h3>userinfo</h3>
-            <a href="https://auth-dev.mozilla.auth0.com/userinfo?access_token=<?php echo $_SERVER['HTTP_OIDC_CLAIM_ACCESS_TOKEN'];?>">https://auth-dev.mozilla.auth0.com/userinfo?access_token=<?php echo $_SERVER['HTTP_OIDC_CLAIM_ACCESS_TOKEN'];?></a>
+            <a href="<?php echo $_SERVER['OIDC_CLAIM_iss']; ?>userinfo?access_token=<?php echo $_SERVER['OIDC_access_token'];?>"><?php echo $_SERVER['OIDC_CLAIM_iss']; ?>userinfo?access_token=<?php echo $_SERVER['OIDC_access_token'];?></a>
 
             <h3>tokeninfo</h3>
-            <a href="https://auth-dev.mozilla.auth0.com/tokeninfo?id_token=<?php echo $_SERVER['HTTP_OIDC_CLAIM_ID_TOKEN'];?>">https://auth-dev.mozilla.auth0.com/tokeninfo?id_token=<?php echo $_SERVER['HTTP_OIDC_CLAIM_ID_TOKEN'];?></a>
+            <a href="<?php echo $_SERVER['OIDC_CLAIM_iss']; ?>tokeninfo?id_token=<?php echo $_SERVER['OIDC_id_token'];?>"><?php echo $_SERVER['OIDC_CLAIM_iss']; ?>tokeninfo?id_token=<?php echo $_SERVER['OIDC_id_token'];?></a>
         </div>
     </body>
 </html>
