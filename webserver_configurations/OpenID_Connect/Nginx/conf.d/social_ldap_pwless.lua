0b56f20cb31bdddbb841eaa9d65cec98bcc7f151
Gene Wood <gene_wood@cementhorizon.com>
2018-01-05 12:45:05 -0800

    Add delegation_endpoint and id_token_renew_delay to social ldap passwordless

--- webserver_configurations/OpenID_Connect/Nginx/conf.d/social_ldap_pwless.lua
+++ webserver_configurations/OpenID_Connect/Nginx/conf.d/social_ldap_pwless.lua
@@ -11,3 +11,7 @@ opts = {
   redirect_after_logout_uri = "https://testrp.security.allizom.org/?uri=https://social-ldap-pwless.testrp.security.allizom.org/",
   refresh_session_interval = 900
 }
+-- Auth0 delegation endpoint
+delegation_endpoint = "https://auth-dev.mozilla.auth0.com/delegation"
+-- How often to renew the id_token in seconds (i.e. when disabled, blocked users will be logged out)
+id_token_renew_delay = 900
