af9b1508f81f7e835a3fd28c1d275565b5dd2123
Gene Wood <gene_wood@cementhorizon.com>
2018-01-05 12:39:25 -0800

    Add social passwordless RP

---
+++ webserver_configurations/OpenID_Connect/Nginx/conf.d/social_pwless.lua
@@ -0,0 +1,17 @@
+-- lua-resty-openidc options
+opts = {
+  redirect_uri_path = "/redirect_uri",
+  discovery = "https://auth-dev.mozilla.auth0.com/.well-known/openid-configuration",
+  client_id = "YOUR CLIENT ID HERE",
+  client_secret = "YOUR CLIENT SECRET HERE",
+  scope = "openid email profile",
+  iat_slack = 600,
+  redirect_uri_scheme = "https",
+  logout_path = "/logout",
+  redirect_after_logout_uri = "https://testrp.security.allizom.org/?uri=https://social-pwless.testrp.security.allizom.org/"
+  refresh_session_interval = 900
+}
+-- Auth0 delegation endpoint
+delegation_endpoint = "https://auth-dev.mozilla.auth0.com/delegation"
+-- How often to renew the id_token in seconds (i.e. when disabled, blocked users will be logged out)
+id_token_renew_delay = 900
